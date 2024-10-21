dbf7bccaa42a1825420b2f27a01511d3948bb6db
Krytoi Xa4 <andrey.grachev.2007@mail.ru>
2024-10-21 20:46:27 +0300

    Added Scanner and new method

---
+++ src/task1/Main.java
@@ -0,0 +1,16 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public static boolean IsDigit(char c) {
+        boolean statement = Character.isAlphabetic(c);
+
+        return !statement;
+    }
+}
