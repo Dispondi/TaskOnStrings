c78be53b8aac1cb2f346d0c45ef968e5d47aa353
Krytoi Xa4 <andrey.grachev.2007@mail.ru>
2024-10-21 20:52:37 +0300

    Finished task

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        char[] c = sc.next().toCharArray();
+
+        if (IsDigit(c[0])) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
     }
 
     public static boolean IsDigit(char c) {
